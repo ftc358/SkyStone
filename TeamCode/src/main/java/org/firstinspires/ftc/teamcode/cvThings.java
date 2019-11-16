c58714b186fd14d1e9a3318d59f952e6f705aa5b
Kevin Wang <kkevinwang2003@gmail.com>
2019-11-15 21:44:50 -0800

    Things happened

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cvThings.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cvThings.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.util.Log;
 
 import com.acmerobotics.dashboard.FtcDashboard;
@@ -30,19 +29,22 @@ import java.util.concurrent.Executors;
 @Config
 @TeleOp
 public class cvThings extends LinearOpMode {
-    OpenCvCamera phoneCam;
-    FtcDashboard dashboard = FtcDashboard.getInstance();
-    Telemetry telemetry = dashboard.getTelemetry();
 
     final private static int frameHeight = 320;
     final private static int frameWidth = 240;
 
-    public static int fromBottom = 0;
+    public static int fromBottom = 5;
     public static int stoneHeight = 40;
 
+    OpenCvCamera phoneCam;
     SamplePipeline samplePipeline;
+
+    // comment out later
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    Telemetry telemetry = dashboard.getTelemetry();
     Bitmap bmp = null;
 
+    // comment out later
     Runnable submitImage = new Runnable() {
         @Override
         public void run() {
@@ -52,27 +54,23 @@ public class cvThings extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         phoneCam = new OpenCvInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
 
         phoneCam.openCameraDevice();
         samplePipeline = new SamplePipeline();
         phoneCam.setPipeline(samplePipeline);
 
+        // comment out later
         ExecutorService networking = Executors.newSingleThreadExecutor();
+
+        phoneCam.startStreaming(frameHeight, frameWidth, OpenCvCameraRotation.UPRIGHT);
 
         waitForStart();
 
         while (opModeIsActive()) {
-
-            if (gamepad1.a) {
-                phoneCam.startStreaming(frameHeight, frameWidth, OpenCvCameraRotation.UPRIGHT);
-            } else if (gamepad1.b) {
-                phoneCam.stopStreaming();
-            } else if (gamepad1.x) {
-                phoneCam.closeCameraDevice();
-            }
-
+            // comment out later
             networking.submit(submitImage);
 
             telemetry.addData("skystonePosition", samplePipeline.getPosition());
@@ -80,63 +78,66 @@ public class cvThings extends LinearOpMode {
         }
     }
 
+    protected class SamplePipeline extends OpenCvPipeline {
 
-    class SamplePipeline extends OpenCvPipeline {
+        private int skystonePosition;
+        Scalar green = new Scalar(0, 0, 255);
 
-        int skystonePosition;
-        Scalar green = new Scalar(0, 255, 0);
+        // defining stone detection zones
+        Rect rectLeft = new Rect(fromBottom, 0, stoneHeight, frameHeight / 3);
+        Rect rectMiddle = new Rect(fromBottom, frameHeight / 3, stoneHeight, frameHeight / 3);
+        Rect rectRight = new Rect(fromBottom, 2 * frameHeight / 3, stoneHeight, frameHeight / 3);
 
         @Override
         public Mat processFrame(Mat input) {
-
-            Rect rectLeft = new Rect(fromBottom, 0, stoneHeight, frameHeight / 3);
-            Rect rectMiddle = new Rect(fromBottom, frameHeight / 3, stoneHeight, frameHeight / 3);
-            Rect rectRight = new Rect(fromBottom, 2 * frameHeight / 3, stoneHeight, frameHeight / 3);
 
             Mat left = new Mat(input, rectLeft);
             Mat middle = new Mat(input, rectMiddle);
             Mat right = new Mat(input, rectRight);
 
-            Scalar leftMean = Core.mean(left);
-            Scalar middleMean = Core.mean(middle);
-            Scalar rightMean = Core.mean(right);
+            double leftValue = getValue(Core.mean(left));
+            double middleValue = getValue(Core.mean(middle));
+            double rightValue = getValue(Core.mean(right));
 
-            float[] leftHsv = new float[3];
-            float[] middleHsv = new float[3];
-            float[] rightHsv = new float[3];
-
-            Color.RGBToHSV((int) leftMean.val[0], (int) leftMean.val[1], (int) leftMean.val[2], leftHsv);
-            Color.RGBToHSV((int) middleMean.val[0], (int) middleMean.val[1], (int) middleMean.val[2], middleHsv);
-            Color.RGBToHSV((int) rightMean.val[0], (int) rightMean.val[1], (int) rightMean.val[2], rightHsv);
-
-            ArrayList<Float> values = new ArrayList<>();
-
-            values.add(leftHsv[2]);
-            values.add(middleHsv[2]);
-            values.add(rightHsv[2]);
+            // make an arrayList with the values of the 3 blocks for easier comparison
+            ArrayList<Double> values = new ArrayList<>();
+            values.add(leftValue);
+            values.add(middleValue);
+            values.add(rightValue);
 
             skystonePosition = values.indexOf(Collections.min(values));
 
             // drawing things on the screen so that things can be seen on screen
-            Imgproc.rectangle(input, rectLeft, green, 1);
-            Imgproc.rectangle(input, rectMiddle, green, 1);
-            Imgproc.rectangle(input, rectRight, green, 1);
+            Imgproc.rectangle(input, rectLeft, green, 2);
+            Imgproc.rectangle(input, rectMiddle, green, 2);
+            Imgproc.rectangle(input, rectRight, green, 2);
 
+            // convert the image to a bitmap to be sent to the dashboard
             try {
                 bmp = Bitmap.createBitmap(input.cols(), input.rows(), Bitmap.Config.ARGB_8888);
                 Utils.matToBitmap(input, bmp);
             } catch (CvException e) {
-                Log.d("OH NO", e.getMessage());
+                Log.d("Bitmap Creation Failure", e.getMessage());
             }
 
             return input;
         }
 
-        public int getPosition() {
+        private int getPosition() {
             return skystonePosition;
         }
 
+        private double getValue(Scalar scalar) {
+            double max = 0;
+
+            for (int i = 0; i < 3; i++) {
+                if (max < scalar.val[i]) {
+                    max = scalar.val[i];
+                }
+            }
+
+            return max;
+        }
 
     }
 }
-
